3aeb2071180ee60f4a8208e7dcec4c462b32506c
Anil Deepak Talathoti <78140366+anil-deepak@users.noreply.github.com>
2021-04-22 11:30:09 -0400

    simple static

--- server/index.js
+++ server/index.js
@@ -3,13 +3,13 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app
-    .get('/', (req, res) => {
+  .use(express.static('./docs'))
+  .get('/', (req, res) => {
     res.send('Hello World!')
-    })
-    .get('/purim', (req, res) => {
-        res.send('Lechaim, to life!');
-    })
-
+  })
+  .get('/purim', (req, res) => {
+    res.send('Lechaim, to life!');
+  })
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
