bf61771c3e00aaaa30a922906369c58b3e9ff277
Anil Deepak Talathoti <78140366+anil-deepak@users.noreply.github.com>
2021-04-22 14:37:34 -0400

    hosting client on node server

--- server/index.js
+++ server/index.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
@@ -10,6 +11,10 @@ app
   .get('/purim', (req, res) => {
     res.send('Lechaim, to life!');
   })
+  // All the way at the end of the pipeline. Return instead of not found
+  .get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../docs/index.html'));
+  })
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
-})
