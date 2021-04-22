ce0051e1371119925bb250f6d974b96542e2b676
Anil Deepak Talathoti <78140366+anil-deepak@users.noreply.github.com>
2021-04-22 11:28:36 -0400

    vue binding

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -2,12 +2,14 @@ import Vue from 'vue';
 import VueRouter from 'vue-router';
 import Home from '../views/Home.vue';
 import Documentation from '@/views/Documentation';
+import MyWall from '@/views/MyWall';
 
 Vue.use(VueRouter)
 
 const routes = [
   { path: '/', name: 'Home', component: Home },
   { path: '/documentation', name: 'Doc', component: Documentation },
+  { path: '/mywall', name: 'MyWall', component: MyWall },
   {
     path: '/about',
     name: 'About',
@@ -19,3 +21,9 @@ const routes = [
     component: () => import(/* webpackChunkName: "secondary" */ '../views/Shop.vue')
   }
 ]
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+})
+export default router
